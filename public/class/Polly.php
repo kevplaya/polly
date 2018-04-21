f9a56f92b484c7e58deb36e08b6274ca951f0192
jihoon.lee <jihoon.lee@rgpkorea.co.kr>
2018-04-21 10:35:27 +0900

    POLLY TO TTS  - make detail of polly

--- public/class/Polly.php
+++ public/class/Polly.php
@@ -8,5 +8,25 @@
 require_once $_SERVER['DOCUMENT_ROOT'].'lib/config.php';
 class Polly
 {
+    protected $oPolly ;
+    
+    public function __construct($sRegion)
+    {
+        $oCredential = new \Aws\Credentials\Credentials(ACCESS_KEY_ID, SECRET_KEY);
+        $aClientInfo = [
+            'version' => '2018–04–21'
+            , 'credentials' => $oCredential
+            , 'region' => $sRegion ];
+        $this->oPolly = new \Aws\Polly\PollyClient($aClientInfo);
+    }
+
+    public function setTextToPolly($sText){
+        $aPollyinfo = [ 'OutputFormat' => 'mp3'
+            , 'Text' => $sText
+            , 'TextType' => 'text'
+            , 'VoiceId' => 'Seoyeon' ];
+        $streamPollydata = $this->oPolly->synthesizeSpeech($aPollyinfo);
+        return $streamPollydata->get('AudioStream')->getContents();
+    }
 
 }
