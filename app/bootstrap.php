5b54f8129d88aab5677988f712a7fbbe41c7b325
jihoon.lee <jihoon.lee@rgpkorea.co.kr>
2018-04-21 09:53:53 +0900

    POLLY TO TTS

---
+++ app/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a201610021
+ * Date: 2018. 4. 21.
+ * Time: AM 9:45
+ */
+
+require_once realpath($_SERVER['DOCUMENT_ROOT'].'/..').'/vendor/autoload.php';
---
+++ public/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a201610021
+ * Date: 2018. 4. 21.
+ * Time: AM 9:51
+ */
+
+class config
+{
+
+}
