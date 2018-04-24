7da9fe614e19800ae40f500880901cb4503941a6
jihoon.lee <jihoon.lee@rgpkorea.co.kr>
2018-04-24 16:18:30 +0900

    POLLY TO TTS  - set Twilio SMS

--- public/class/Twilio.php
+++ public/class/Twilio.php
@@ -6,7 +6,30 @@
  * Time: AM 10:36
  */
 
+require_once $_SERVER['DOCUMENT_ROOT'].'/lib/config.php';
+
+
 class Twilio
 {
+    protected $oClient;
+    public  function __construct()
+    {
+        $sSid = '';
+        $sToken = '';
+        $this->oClient = new Twilio\Rest\Client($sSid, $sToken);
 
+    }
+
+    public function setMessage(){
+
+        $this->oClient->messages->create(
+
+            '+8201071186827',
+            array(
+
+                'from' => '+8201071186827',
+                'body' => 'Test SMS!'
+            )
+        );
+    }
 }
