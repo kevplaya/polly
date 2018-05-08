29c555aa1e47ca84406971f01b630503f56dd06b
jihoon.lee <jihoon.lee@rgpkorea.co.kr>
2018-05-08 18:04:08 +0900

    POLLY TO TTS  - organizing directories

--- public/TEST.php
+++ public/TEST.php
@@ -11,6 +11,7 @@ require_once 'lib/config.php';
 $oPolly = new Helper\AWS\Polly('ap-northeast-2');
 $oS3 = new Helper\AWS\S3('pollymp3-bdt','ap-northeast-2');
 $oTwilio = new Helper\TWILIO\Voice();
+
 $resultData =  $oPolly->setTextToPolly($argv[1]);
 
 $sS3link = $oS3->putBucket(time().'-order_no.mp3',$resultData,'audio/mpeg');
